86520dad877e390fda884dcb4e213dac9cf91014
oyassine1911 <thvas1911@gmail.com>
2023-01-10 04:54:12 -0700

    finished the res of the code and cleaned up the code

--- index.js
+++ index.js
@@ -29,4 +29,55 @@ const questions = [
     name: "license",
     choices: ["MIT", "ISC", "GPL-3.0", "Apache-2.0"],
   },
-  
+  {
+    type: "input",
+    message: "Provide the contribution guidelines for your project?",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Provide the test instructions for your project?",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "Enter your GitHub username",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "Enter your email address",
+    name: "email",
+    },
+      {
+        type: "input",
+        message: "Enter the file path for your screenshot or gif (optional)",
+        name: "screenshot",
+        validate: function(value) {
+            if(!value) return true; // if no path is provided, it's optional.
+            if(!fs.existsSync(value)) return 'file not found';
+            if (!/(\.jpg|\.jpeg|\.png|\.gif)$/i.test(value)) return 'file is not an image';
+            return true;
+        }
+    }
+];
+
+async function writeToFile(fileName, data) {
+  try {
+    await fs.promises.writeFile(fileName, data);
+    console.log(`Successfully wrote to ${fileName}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function init() {
+  if (fs.existsSync("README.md")) {
+    fs.unlinkSync("README.md");
+  }
+  const answers = await inquirer.prompt(questions);
+  const markdown = generateMarkdown(answers);
+  await writeToFile("README.md", markdown);
+}
+
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,27 @@ function generateMarkdown(data) {
     markdown += `## Usage\n${data.usage}\n`;
   }
 
+  if (data.license) {
+    markdown += `## License\n`;
+    markdown += `[![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})\n`;
+    markdown += `This project is licensed under the [${data.license}](https://opensource.org/licenses/${data.license}) license.\n`;
+  }
 
+  if (data.contributing) {
+    markdown += `## Contributing\n${data.contributing}\n`;
+  }
+
+  if (data.tests) {
+    markdown += `## Tests\n${data.tests}\n`;
+  }
+
+  markdown += `## Contact Information\n- **GitHub username:** ${data.username}\n- **Email:** ${data.email}\n`;
+
+  if (data.screenshot) {
+        markdown += `## Screenshot\n![Screenshot](${data.screenshot})\n`;
+    }
+
+  return markdown;
 }
 
 module.exports = generateMarkdown;
